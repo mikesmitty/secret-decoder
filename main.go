899207e19b90a4ba2bb96d6ed0c9ac3ebe0e2be6
Michael Smith <michael.smith@wellsky.com>
2022-05-10 17:45:00 -0400

    Initial commit

---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/base64"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"reflect"
+
+	"gopkg.in/yaml.v2"
+)
+
+func main() {
+	secret := make(map[string]interface{})
+
+	stdin, err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		log.Fatal("error: %w", err)
+	}
+
+	err = yaml.Unmarshal(stdin, &secret)
+	if err != nil {
+		log.Fatal("error: %w", err)
+	}
+
+	stringData := make(map[string]string)
+	iter := reflect.ValueOf(secret["data"]).MapRange()
+	for iter.Next() {
+		key := iter.Key().Interface().(string)
+		value := iter.Value().Interface().(string)
+		item, err := base64.StdEncoding.DecodeString(string(value))
+		if err != nil {
+			log.Fatal("error: %w", err)
+		}
+		stringData[key] = string(item)
+	}
+
+	delete(secret, "data")
+	secret["stringData"] = stringData
+
+	output, err := yaml.Marshal(&secret)
+	if err != nil {
+		log.Fatal("error: %w", err)
+	}
+
+	fmt.Print(string(output))
+}
